a9857a8d13c94fd681bdbd9050298f5e27a056fa
cabral2 <saviocampolina9@gmail.com>
2021-07-05 18:02:12 -0300

    Basic routing

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,19 @@
-const Header = () => {};
+import { Link } from "react-router-dom";
 
-export { Header };
+const Header = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/users">Users</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Header;
