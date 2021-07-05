81f391c70e9a929bd3c05f35acb102a775ed856d
cabral2 <saviocampolina9@gmail.com>
2021-07-05 18:02:12 -0300

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./assets/logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <Header className="App-header"></Header>
+            <Switch>
+                <Route path="/" exact>
+                    {/* Component */}
+                </Route>
+                <Route path="/home">{/* Component */}</Route>
+                <Route path="/users">{/* Component */}</Route>
+            </Switch>
+            <footer />
         </div>
     );
 }
