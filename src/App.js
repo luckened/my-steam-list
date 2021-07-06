dfd6d8963e3dcc01d2456d9d7a634576c3a294ae
Davi Emediato <48991554+daviemediato@users.noreply.github.com>
2021-07-05 22:51:51 -0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./assets/logo.svg";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header";
@@ -7,14 +6,14 @@ import Footer from "./components/footer";
 function App() {
     return (
         <div className="App">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <Header className="App-header"></Header>
+            <Header />
             <Switch>
                 <Route path="/" exact>
                     {/* Component */}
                 </Route>
                 <Route path="/home">{/* Component */}</Route>
                 <Route path="/users">{/* Component */}</Route>
+                <Route path="/mylist">{/* Component */}</Route>
             </Switch>
             <Footer />
         </div>
