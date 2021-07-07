83756c4e305b960598d2fdd298ee6f5ab0a67151
Lucca Miranda <luccafmsilva@gmail.com>
2021-07-07 00:58:57 -0300

    feat: font faces

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/reset.css";
+import "./styles/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
