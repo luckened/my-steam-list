910701fb4758b2e2f1349f1979114fd3ecc71502
Lucca Miranda <luccafmsilva@gmail.com>
2021-07-07 22:33:34 -0300

    feat: dark mode button

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,15 +1,34 @@
 import styles from "./index.module.css";
 import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
-const Footer = () => 
-    <footer className={styles.footer}>
+const Footer = ({ darkMode }) => (
+    <footer className={`${styles.footer} ${darkMode ? styles.footerDark : ""}`}>
         <span>mySteamList © todos os direitos reservados.</span>
         <span>feito com ❤️ &nbsp;por d1sn3y</span>
         <span className={styles.icons}>
-            <a className={styles.icons} href="https://www.instagram.com/luckened" target="_blank" ><FaInstagram size={30} color="white"/></a>
-            <a className={styles.icons} href="https://www.google.com" target="_blank"><FaYoutube size={30} color="white"/></a>
-            <a className={styles.icons} href="https://www.google.com" target="_blank"><FaTwitter size={30} color="white"/></a>
+            <a
+                className={styles.icons}
+                href="https://www.instagram.com/luckened"
+                target="_blank"
+            >
+                <FaInstagram size={30} color="white" />
+            </a>
+            <a
+                className={styles.icons}
+                href="https://www.google.com"
+                target="_blank"
+            >
+                <FaYoutube size={30} color="white" />
+            </a>
+            <a
+                className={styles.icons}
+                href="https://www.google.com"
+                target="_blank"
+            >
+                <FaTwitter size={30} color="white" />
+            </a>
         </span>
     </footer>
+);
 
 export default Footer;
