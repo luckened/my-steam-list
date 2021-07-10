1fe44c6b2d78046b718625d91356032090e3e2d2
Lucca Miranda <luccafmsilva@gmail.com>
2021-07-10 17:01:50 -0300

    feat: profile page

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,25 +1,31 @@
 import UserData from "../../components/userData";
 import styles from "./index.module.css";
+import { FaEdit } from "react-icons/fa";
+import { useState } from "react";
 
-const Profile = ({ userId, isMyProfile }) => {
+const Profile = ({ userId, isMyProfile, darkMode }) => {
+    const [isEditMode, setIsEditMode] = useState(false);
+
     return (
         <div className={styles.profileContainer}>
             <div className={styles.pageHeader}>
                 <h1 className={styles.pageName}>
-                    {isMyProfile ? "Meu perfil" : "Perfil"}
+                    {isMyProfile ? "My profile" : "Profile"}
                 </h1>
                 {isMyProfile && (
-                    <button className={styles.editProfile} onClick={false}>
-                        Editar
+                    <button onClick={() => setIsEditMode(!isEditMode)}>
+                        <FaEdit color="white" size={25} />
                     </button>
                 )}
             </div>
             <div>
                 <UserData
                     imgLink="https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg?quality=70&strip=info&resize=680,453"
-                    name="savinho"
+                    userName="savinho"
                     email="savinhord@gmail"
-                    description="salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. "
+                    description="salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha.salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. salve salve yodinha. "
+                    darkMode={darkMode}
+                    isEditMode={isEditMode}
                 />
             </div>
         </div>
