bb6716138780bdd73208d29ba1af7b2b515ff61b
Lucca Miranda <luccafmsilva@gmail.com>
2021-07-10 17:01:50 -0300

    feat: profile page

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
                 <Route path="/users">{/* Component */}</Route>
                 <Route path="/home">{/* Component */}</Route>
                 <Route path="/profile">
-                    <Profile isMyProfile={true} />
+                    <Profile darkMode={darkMode} isMyProfile={true} />
                 </Route>
                 <Route path="/mylist">{/* Component */}</Route>
             </Switch>
