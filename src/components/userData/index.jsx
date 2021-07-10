e2113fbdb05e802ba6d3ffcbd5cd3f2b2a18830e
Lucca Miranda <luccafmsilva@gmail.com>
2021-07-10 17:01:50 -0300

    feat: profile page

--- src/components/userData/index.jsx
+++ src/components/userData/index.jsx
@@ -1,24 +1,88 @@
 import styles from "./index.module.css";
+import { FaEdit } from "react-icons/fa";
 
-const UserData = ({ name, email, description, imgLink }) => {
-    return (
-        <>
-            <div className={styles.userDataContainer}>
-                <img
-                    className={styles.profileImage}
-                    src={imgLink}
-                    alt="Profile"
-                />
-                <div className={styles.profileTextContainer}>
-                    <p className={styles.profileText}>{name}</p>
-                    <p className={styles.profileText}>{email}</p>
-                    <p className={styles.profileText}>
-                        Descrição: {description}
-                    </p>
-                </div>
-            </div>
-        </>
-    );
-};
+const UserData = ({
+    userName,
+    email,
+    description,
+    imgLink,
+    darkMode,
+    isEditMode,
+}) => (
+    <div
+        className={`${styles.userDataContainer} ${
+            darkMode ? styles.userDataContainerDark : ""
+        }`}
+    >
+        <div>
+            <img
+                className={styles.profileImage}
+                src={imgLink}
+                alt={`${userName}'s profile picture`}
+            />
+            {isEditMode && (
+                <>
+                    <label
+                        for="upload-photo"
+                        className={styles.profileEditButton}
+                    >
+                        <FaEdit color="white" size={25} />
+                        edit profile picture
+                    </label>
+                    <input
+                        id="upload-photo"
+                        accept="image/png, image/jpeg"
+                        className={styles.uploadPhoto}
+                        type="file"
+                    ></input>
+                </>
+            )}
+        </div>
+        <div
+            className={`${styles.profileTextContainer} ${
+                darkMode ? styles.profileTextContainerDark : ""
+            }`}
+        >
+            {isEditMode ? (
+                <>
+                    <input
+                        defaultValue={userName}
+                        placeholder="Username"
+                        className={styles.profileUsername}
+                    />
+                    <input
+                        defaultValue={email}
+                        type="email"
+                        placeholder="E-mail"
+                        className={styles.profileEmail}
+                    />
+                    <textarea
+                        rows={20}
+                        defaultValue={`bio: ${description}`}
+                        type="textarea"
+                        placeholder="User bio"
+                        className={styles.profileBio}
+                    />
+                </>
+            ) : (
+                <>
+                    <p className={styles.profileUsername}>{userName}</p>
+                    <p className={styles.profileEmail}>{email}</p>
+                    <p className={styles.profileBio}>bio: {description}</p>
+                </>
+            )}
+            {isEditMode && (
+                <button
+                    onClick={() => alert("updateprofile post")}
+                    className={`${styles.updateProfileButton} ${
+                        darkMode ? styles.updateProfileButtonDark : ""
+                    }`}
+                >
+                    update profile
+                </button>
+            )}
+        </div>
+    </div>
+);
 
 export default UserData;
