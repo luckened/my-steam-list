6f94e69731ddf0245faea102874844c9ef8ee215
Lucca Miranda <lucca.miranda@buser.com.br>
2021-08-24 21:55:17 -0300

    feat: click no card adiciona na minha lista

--- src/components/gameCard/index.jsx
+++ src/components/gameCard/index.jsx
@@ -1,11 +1,22 @@
 import styles from "./index.module.css";
 import GameTag from "../gameTag";
+import { FaPlus } from "react-icons/fa";
 
 const GameCard = (props) => {
     const { name, image, tagList } = props;
     const tagGroup = tagList.map((name) => <GameTag name={name} />);
     return (
-        <span className={styles.card}>
+        <span
+            onClick={() => {
+                alert(
+                    `chama a query pra adicionar o jogo ${name} na lista do usuario`
+                );
+            }}
+            className={styles.card}
+        >
+            <div className={styles.plusIcon}>
+                <FaPlus size={40} color="white" />
+            </div>
             <img className={styles.image} src={image} alt={`${name}'s`} />
             <div className={styles.title}>{name}</div>
             <div className={`${styles.tagGroup} ${styles.scrollbar}`}>
